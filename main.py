0a648e53aeeab8ac6da7baeb4656f8c08988b9de
MarcosVianaDev <marcos.viana.dev@gmail.com>
2023-09-22 17:03:33 -0300

    API get route complete

--- main.py
+++ main.py
@@ -1,66 +1,15 @@
 from typing import Annotated
 
-from fastapi import (FastAPI, Query, WebSocket, WebSocketException,
-                     status)
-from fastapi.responses import HTMLResponse  # , RedirectResponse, JSONResponse
+from fastapi import (FastAPI, WebSocket, WebSocketException, HTTPException, status)
+from fastapi.responses import HTMLResponse, JSONResponse
 
 from fastapi.middleware.cors import CORSMiddleware
-# from replit import db #banco de dados da ReplIt
-import uvicorn  # para rodar o server
+
+import barber_db_data as db
 
 ORIGINS = ["*"]
 METHODS = ["*"]
 HEADERS = ["*"]
-
-html = """
-<!DOCTYPE html>
-<html>
-
-<head>
-<title>Chat com FastAPI</title>
-</head>
-
-<body>
-<h1>Chat com FastAPI usando WebSocket</h1>
-<form onsubmit="sendMessage(event)">
-<label>Item ID: <input type="text" id="itemId" autocomplete="off" value="foo"></label>
-<label>Token: <input type="text" id="token" autocomplete="off" value="some-key-token"></label>
-<button onclick="connect(event)">Conectar</button>
-<hr>
-<label>Mensagem: <input type="text" id="messageText" autocomplete="off" /></label>
-<button>Enviar</button>
-</form>
-<ul id='messages'>
-</ul>
-<script>
-const hostProtocol = (window.location.host == '0.0.0.0:8080' || window.location.host == 'localhost:8080') ? 'ws' : 'wss'
-var ws = null;
-function connect(event) {
-var itemId = document.getElementById('itemId')
-var token = document.getElementById('token')
-ws = new WebSocket(`${hostProtocol}://${window.location.host}/items/${itemId.value}/ws?token=${token.value}`);
-
-ws.onmessage = function (event) {
-let messages = document.getElementById('messages');
-let message = document.createElement('li');
-let content = document.createTextNode(event.data);
-message.appendChild(content)
-messages.appendChild(message)
-}
-event.preventDefault()
-}
-
-function sendMessage(event) {
-const input = document.getElementById("messageText")
-ws.send(input.value)
-input.value = ''
-event.preventDefault()
-}
-</script>
-</body>
-
-</html>
-"""
 
 app = FastAPI()
 
@@ -73,27 +22,36 @@ app.add_middleware(
 )
 
 
-@app.get("/")
-async def get():
-  return HTMLResponse(content=html, status_code=200)
+# @app.get("/")
+# async def get():
+#     return HTMLResponse(content=html, status_code=200)
 
-@app.websocket("/items/{item_id}/ws")
-async def websocket_endpoint(*,
-                             websocket: WebSocket,
-                             item_id: str,
-                             q: int | None = None,
-                             token: Annotated[str | None, Query()] = None):
-  await websocket.accept()
-  while True:
-    data = await websocket.receive_text()
-    await websocket.send_text(
-        f"Valor do token é: {token}")
-    if q is not None:
-      await websocket.send_text(f'Valor do parametro Query "q" é: {q}')
-    await websocket.send_text(
-        f'Mensagem de texto: {data}, para o item: {item_id}')
+
+@app.get("/{aba}/{id}")
+async def getData(aba:str, id:str):
+    resp = db.getData(aba=aba, id=id)
+    if resp:
+        return JSONResponse(resp)
+    return JSONResponse(content = f'Id {id} not found' ,status_code=404)
+
+# @app.websocket("/items/{item_id}/ws")
+# async def websocket_endpoint(*,
+#                              websocket: WebSocket,
+#                              item_id: str,
+#                              q: int | None = None,
+#                              token: Annotated[str | None, Query()] = None):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await websocket.send_text(
+#             f"Valor do token é: {token}")
+#         if q is not None:
+#             await websocket.send_text(f'Valor do parametro Query "q" é: {q}')
+#         await websocket.send_text(
+#             f'Mensagem de texto: {data}, para o item: {item_id}')
 
 
 if __name__ == "__main__":
-  print('Runing...')
-  uvicorn.run(app, port=8080, host="0.0.0.0")
+    import uvicorn  # para rodar o server
+    print('Runing...')
+    uvicorn.run(app, port=8080, host="0.0.0.0")
